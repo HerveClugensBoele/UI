ab191a8b578a720684cfeb5664e1cadc12cb0883
1amchris <beaulieu.christophe23@gmail.com>
2020-02-21 17:02:15 -0500

    Working on comparer.html/css/js

---
+++ comparer/comparer.js
@@ -0,0 +1,9 @@
+//  <section>
+//      <article class="category-header">
+//          <span>[CATEGORY TITLE GOES HERE]</span>
+//          <i class="material-icons" onclick="toggleStar(this)">star_border</i>
+//      </article>
+//      <article class="category-paragraph">
+//          <p>[CATEGORY INFO GOES HERE]</p>
+//      </article>
+//  </section>
